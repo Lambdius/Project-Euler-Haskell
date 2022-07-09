0d5633e668cc33b5593a00c695bfa72585c340ca
Lambdius <JavaScriptizer101@yandex.ru>
2022-07-09 19:14:10 +0300

    problem 3

---
+++ 3_problem.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE MultiWayIf #-}
+
+
+--
+
+
+
+
+-- best practice
+lpf :: Integer -> Integer -> Integer -> Integer
+lpf n d a = if 
+    | d * d <= n && mod n d == 0 -> lpf ( div n d ) d d
+    | d * d <= n -> lpf n ( d + 1 ) a
+    | n > 1 -> n
+    | otherwise -> a
+        
+
+pa'lpf :: Integer -> Integer
+pa'lpf n = lpf n 2 2
+
+main :: IO ()
+main =  do
+print( pa'lpf 600851475143 )
